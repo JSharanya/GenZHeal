a9fdd40843c19e39896d64939da8cb5f05fbb181
Mathu <mathumilalogan@gmail.com>
2024-06-03 13:06:28 +0530

    minor changes in appointment

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,70 +1,71 @@
-import React,{useEffect,useState,useRef} from 'react'
+import React,{useEffect,useState,useRef} from "react";
+import styled from "styled-components";
 
 
 const Stepper = ({steps,currentStep}) => {
-      const[newStep,setNewStep] = useState([])
-      const stepRef = useRef()
+      const[newStep,setNewStep] = useState([]);
+      const stepRef = useRef();
 
-      const updateStep = (stepNumber,steps)=>{
+      const updateStep = (stepNumber, steps) => {
           const newSteps =[...steps]
           let count =0;
           while(count < newSteps.length){
            // current step 
-              if(count==stepNumber){
-                newSteps[count]={
+              if(count===stepNumber){
+                newSteps[count] ={
                   ...newSteps[count],
-                  highlighted:true,
-                  selected:true,
-                  completed:true,
-                }
+                  highlighted: true,
+                  selected: true,
+                  completed: true,
+                };
                 count++;
               }
            //step completed
-           else if(count<stepNumber){
+           else if(count < stepNumber){
             newSteps[count]={
               ...newSteps[count],
-              highlighted:false,
-              selected:true,
-              completed:true,
-            }
+              highlighted: false,
+              selected: true,
+              completed: true,
+            };
             count++;
            }
            //step pending
            else{
             newSteps[count]={
               ...newSteps[count],
-              highlighted:false,
-              selected:false,
-              completed:false,
-            }
+              highlighted: false,
+              selected: false,
+              completed: false,
+            };
             count++;
            }
           }
           return newSteps;
-      }
-    useEffect(()=>{
+      };
+    useEffect(() => { 
       //create object
-      const stepState =steps.map((step,index)=>
-      Object.assign({},{
+      const stepsState =steps.map((step,index)=>
+      Object.assign({}, {
         description: step,
         completed: false,
-        highlighted: index == 0 ? true : false,
-        selected: index == 0 ? true : false,
+        highlighted: index === 0 ? true : false,
+        selected: index === 0 ? true : false,
       }
     )
-   )
-      stepRef.current = stepState;
+   );
+      stepRef.current = stepsState;
       const current = updateStep(currentStep -1, stepRef.current);
       setNewStep(current);
 
-    },[steps,currentStep])
+    },[steps,currentStep]);
 
   const displaySteps = newStep.map((step, index)=>{
 return(
   
     <div key ={index}
      className={
-          index !==newStep.length -1 ? 'w-full flex items-center' : 'flex items-center'}>
+          index !== newStep.length -1 ? 'w-full flex items-center' : 'flex items-center'}>
          <div className='relative flex flex-col items-center text-teal-600'>
           <div className={'rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3 ${step.selected ? "bg-green-600 text-white font-bold border border-green-600" : ""}'}> 
            {/*Display number*/} 
@@ -75,6 +76,7 @@ return(
             )}
           </div>
        <div className={'absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase ${step.highlighted ? "text-gray-900" : "text-gray-400"}'}>
+        {/*Display description*/}
           {step.description}
        </div> 
        </div>
@@ -82,8 +84,8 @@ return(
           {/*display line*/}
        </div>
     </div>
-  )
-  })
+  );
+  });
   
   return (
     <div className='mx-4 p-4 flex justify-between items-center'>
@@ -91,7 +93,7 @@ return(
     </div>
     
 
-  )
-}
+  );
+};
 
-export default Stepper
+export default Stepper;
